44dc057383aab24112f1bf5b4ae964283dbdfd13
minhtai <conian2505@gmail.com>
2022-10-09 00:42:51 +0700

    Auth-Login-Redux

--- React/src/features/User/UserApi.ts
+++ React/src/features/User/UserApi.ts
@@ -8,11 +8,21 @@ export const registerUser = async (payload: UserDataTypes) => {
 
 export const loginUser = async (payload: UserDataTypes) => {
   const res = await apiClient.post(`/api/v1/sign_in`, payload);
+
   return res.data;
 };
 
 export const loadUser = async () => {
-  const res = await apiClient.get(`/api/v1/users`);
-  console.log(res.data);
-  return res.data;
+  try {
+    const token = JSON.parse(sessionStorage.getItem("access_token") || "");
+
+    const res = await apiClient.get(`/api/v1/load-user`, {
+      headers: {
+        Authorization: "bearer " + token,
+      },
+    });
+    return res.data;
+  } catch (error: any) {
+    return error.response.data;
+  }
 };
