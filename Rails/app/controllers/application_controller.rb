5d8d0cc39ae5345d9c425e93fa2472048c04c460
Truong Tho <tho.trannhattruong@gmail.com>
2022-10-13 22:30:12 +0700

    update authen token

--- Rails/app/controllers/application_controller.rb
+++ Rails/app/controllers/application_controller.rb
@@ -2,14 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   acts_as_token_authentication_handler_for User, {fallback: :none}
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user_from_token
 
   private
-  def authenticate_user_from_token
-    render json: {message: "You are not authenticated"},
-      status: 401 if current_user.nil?
-  end
-
   def ensure_params_exist
     if params[:user].blank?
       render json: {
