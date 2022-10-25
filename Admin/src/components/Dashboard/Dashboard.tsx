eeced980f57643697129b8314af73bad7d5e60e7
minhtai <conian2505@gmail.com>
2022-10-25 09:53:20 +0700

    Create-Branch-Admin

---
+++ Admin/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./dashboardStyles.scss";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return <div className={"dashboard"}>Dashboard</div>;
+};
+
+export default Dashboard;
