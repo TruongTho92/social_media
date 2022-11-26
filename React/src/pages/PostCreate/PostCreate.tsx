b971d7f60bb4e1aedc8ab7258948e9cab311382d
minhtai <conian2505@gmail.com>
2022-11-26 21:36:46 +0700

    Update-UI

--- React/src/pages/PostCreate/PostCreate.tsx
+++ React/src/pages/PostCreate/PostCreate.tsx
@@ -14,7 +14,10 @@ import {
 } from "~/components/ArrowSlickCustom/ArrowSlickCustom";
 import Header from "~/components/Header";
 import { postsApi } from "~/features/accountPost/Posts/postsApi";
-import { getLoadingPosts } from "~/features/accountPost/Posts/postsSlice";
+import {
+  getLoadingCreate,
+  getLoadingPosts,
+} from "~/features/accountPost/Posts/postsSlice";
 import { getUser } from "~/features/Auth/userSlice";
 
 import styles from "./postCreate.module.scss";
@@ -22,12 +25,11 @@ import styles from "./postCreate.module.scss";
 const PostCreate: React.FC = () => {
   const dispatch = useAppDispatch();
   const getUserData = useAppSelector(getUser);
-  const getLoadingPost = useAppSelector(getLoadingPosts);
-
+  const getLoadingPost = useAppSelector(getLoadingCreate);
+  const [openLoading, setOpenLoading] = useState(false);
   const [image, setImage] = useState("");
   const [caption, setCaption] = useState("");
   const [isHasImage, setIsHasImage] = useState(false);
-
   const [mutipleImage, setMutipleImage] = useState<any[]>([]);
   const [imageURLS, setImageURLs] = useState<any[]>([]);
 
@@ -68,6 +70,12 @@ const PostCreate: React.FC = () => {
     });
   }, [mutipleImage]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setOpenLoading(false);
+    }, 3000);
+  }, [getLoadingPost]);
+
   const hanleCreatePost = async () => {
     const payload: PostPayloadCreate = {
       post: {
@@ -75,8 +83,8 @@ const PostCreate: React.FC = () => {
         caption,
       },
     };
-
     await dispatch(postsApi.create(payload));
+    setOpenLoading(true);
     setIsHasImage(false);
     setImageURLs([]);
     setCaption("");
@@ -94,84 +102,95 @@ const PostCreate: React.FC = () => {
       <Header />
       <div className={`${styles.createPost}`}>
         <div className={styles.createPostContainer}>
-          <div className={styles.createPostHeader}>
-            <Link to="/" style={{ lineHeight: 0 }}>
-              <HiOutlineArrowLeft className={styles.iconBack} />
-            </Link>
-            <Typography className={styles.heading}>Create new post</Typography>
-            <div onClick={hanleCreatePost}>
-              <Typography className={styles.share}>Share</Typography>
-            </div>
-          </div>
-
-          {/* FORM */}
-          <Form className={styles.form} onFinish={hanleCreatePost}>
-            <Form.Item className={`${styles.formItem} ${styles.widthContent}`}>
-              <label htmlFor="input-image" className={styles.labelInputImage}>
-                <MdDriveFolderUpload className={styles.uploadIcon} />
-                <span>
-                  {isHasImage ? "Change Image" : "Upload Your images"}
-                </span>
-              </label>
-              <Input
-                type="file"
-                id="input-image"
-                className={styles.inputForm}
-                onChange={handleChangeMutipleImg}
-                hidden
-                multiple
-              />
-
-              {/* MUTIPLE IMAGE */}
-              <Slider {...settings}>
-                {imageURLS.map((image) => (
-                  <div className={styles.imagePost} key={image}>
-                    <img key={image} src={image} alt="" />
-                  </div>
-                ))}
-              </Slider>
-            </Form.Item>
-            <Form.Item className={`${styles.formItem} ${styles.widthFull}`}>
-              {/* AVATAR */}
-              <div className={styles.account}>
-                <div className={styles.imageAccount}>
-                  <img
-                    src={
-                      getUserData.user.avatar
-                        ? getUserData.user.avatar
-                        : "/assets/images/user-img.jpg"
-                    }
-                    alt="avatar_account"
-                  />
+          {openLoading ? (
+            <>
+              {getLoadingPost ? (
+                <div className={styles.loadingCreate}>
+                  <img src="/assets/images/loading.gif" alt="" />
                 </div>
-                <Typography className={styles.nameAccount}>Minh Tai</Typography>
+              ) : (
+                <div className={`${styles.loadingCreate}`}>
+                  <img src="/assets/images/success.gif" alt="" />
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              <div className={styles.createPostHeader}>
+                <Link to="/" style={{ lineHeight: 0 }}>
+                  <HiOutlineArrowLeft className={styles.iconBack} />
+                </Link>
+                <Typography className={styles.heading}>
+                  Create new post
+                </Typography>
+                <div onClick={hanleCreatePost}>
+                  <Typography className={styles.share}>Share</Typography>
+                </div>
               </div>
 
-              {/* INPUT */}
-              <Input.TextArea
-                value={caption}
-                className={styles.inputForm}
-                placeholder="Write a caption..."
-                onChange={(e) => setCaption(e.target.value)}
-              />
-            </Form.Item>
-          </Form>
+              {/* FORM */}
+              <Form className={styles.form} onFinish={hanleCreatePost}>
+                <Form.Item
+                  className={`${styles.formItem} ${styles.widthContent}`}
+                >
+                  <label
+                    htmlFor="input-image"
+                    className={styles.labelInputImage}
+                  >
+                    <MdDriveFolderUpload className={styles.uploadIcon} />
+                    <span>
+                      {isHasImage ? "Change Image" : "Upload Your images"}
+                    </span>
+                  </label>
+                  <Input
+                    type="file"
+                    id="input-image"
+                    className={styles.inputForm}
+                    onChange={handleChangeMutipleImg}
+                    hidden
+                    multiple
+                  />
+
+                  {/* MUTIPLE IMAGE */}
+                  <Slider {...settings}>
+                    {imageURLS.map((image) => (
+                      <div className={styles.imagePost} key={image}>
+                        <img key={image} src={image} alt="" />
+                      </div>
+                    ))}
+                  </Slider>
+                </Form.Item>
+                <Form.Item className={`${styles.formItem} ${styles.widthFull}`}>
+                  {/* AVATAR */}
+                  <div className={styles.account}>
+                    <div className={styles.imageAccount}>
+                      <img
+                        src={
+                          getUserData.user.avatar
+                            ? getUserData.user.avatar
+                            : "/assets/images/user-img.jpg"
+                        }
+                        alt="avatar_account"
+                      />
+                    </div>
+                    <Typography className={styles.nameAccount}>
+                      Minh Tai
+                    </Typography>
+                  </div>
+
+                  {/* INPUT */}
+                  <Input.TextArea
+                    value={caption}
+                    className={styles.inputForm}
+                    placeholder="Write a caption..."
+                    onChange={(e) => setCaption(e.target.value)}
+                  />
+                </Form.Item>
+              </Form>
+            </>
+          )}
         </div>
       </div>
-
-      <ToastContainer
-        icon={<MdOutlineDone size={30} />}
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </>
   );
 };
